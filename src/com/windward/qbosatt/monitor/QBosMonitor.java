d5a2498cb396f3ce23649580111b948c51fa4cd9
Andrew Glover <ajglover@gmail.com>
2013-08-27 11:23:21 -0400

    init commit

---
+++ src/com/windward/qbosatt/monitor/QBosMonitor.java
@@ -0,0 +1,22 @@
+package com.windward.qbosatt.monitor;
+
+import com.realops.foundation.adapterframework.AbstractMonitorAdapter;
+import com.realops.foundation.adapterframework.AdapterException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aglover
+ * Date: 8/27/13
+ * Time: 11:20 AM
+ */
+public class QBosMonitor extends AbstractMonitorAdapter {
+    @Override
+    public void shutdown() throws AdapterException {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
