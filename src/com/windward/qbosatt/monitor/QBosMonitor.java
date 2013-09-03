94410e4fba03ce955f38946f0a7532e62acd0655
Andrew Glover <ajglover@gmail.com>
2013-09-03 10:04:18 -0400

    added some logging

--- src/com/windward/qbosatt/monitor/QBosMonitor.java
+++ src/com/windward/qbosatt/monitor/QBosMonitor.java
@@ -8,6 +8,7 @@ import com.realops.common.xml.XML;
 import com.realops.foundation.adapterframework.AbstractMonitorAdapter;
 import com.realops.foundation.adapterframework.AdapterException;
 import com.realops.foundation.adapterframework.AdapterManager;
+import org.apache.log4j.Logger;
 
 import java.io.StringReader;
 
@@ -19,6 +20,7 @@ import java.io.StringReader;
  */
 public class QBosMonitor extends AbstractMonitorAdapter {
 
+    private static Logger LOGGER = Logger.getLogger(QBosMonitor.class);
     private QBosMonitorConfiguration configuration;
     private boolean continueToMonitor;
     private SQSAdapter ahoy;
@@ -30,24 +32,29 @@ public class QBosMonitor extends AbstractMonitorAdapter {
         }
         continueToMonitor = true;
         setState(StateEnum.STARTING);
+        LOGGER.debug("QBosMonitor has initialized");
     }
 
     @Override
     public void shutdown() throws AdapterException {
         continueToMonitor = false;
         setState(StateEnum.STOPPING);
+        LOGGER.debug("QBosMonitor has shutdown");
     }
 
     @Override
     public void run() {
+        LOGGER.debug("QBosMonitor run method invoked");
         setState(StateEnum.RUNNING);
         ahoy = getSqsAdapter();
 
         while (continueToMonitor && getState() == StateEnum.RUNNING) {
+            LOGGER.debug("QBosMonitor is in while loop, about to invoke ahoy.receive");
             ahoy.receive(new MessageReceivedCallback() {
                 @Override
                 public void onReceive(String id, String body) {
                     try {
+                        LOGGER.debug("onReceive callback invoked! About to send event for this body: " + body);
                         sendEvent("QBos", XML.read(new StringReader(body)));
                     } catch (InvalidXMLFormatException e) {
                         //TODO: how does one handle an error when parsing XML inside a monitor?
@@ -59,6 +66,7 @@ public class QBosMonitor extends AbstractMonitorAdapter {
             try {
                 Thread.sleep(60000); //1 minute
             } catch (InterruptedException e) {
+                LOGGER.error("Exception in run() -- bad news", e);
                 //bad issue, bail out
                 continueToMonitor = false;
                 setState(StateEnum.FAULT);
