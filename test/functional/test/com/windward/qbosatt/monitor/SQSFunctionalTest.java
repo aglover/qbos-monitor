22a3a0e5c6e074f9dcb158271779a8cce7e0403b
Andrew Glover <ajglover@gmail.com>
2013-09-04 21:20:21 -0400

    Added functional test

---
+++ test/functional/test/com/windward/qbosatt/monitor/SQSFunctionalTest.java
@@ -0,0 +1,58 @@
+package test.com.windward.qbosatt.monitor;
+
+import com.b50.sqs.MessageReceivedCallback;
+import com.b50.sqs.MessageSentCallback;
+import com.b50.sqs.SQSAdapter;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aglover
+ * Date: 9/4/13
+ * Time: 9:14 PM
+ */
+public class SQSFunctionalTest {
+    @Test
+    public void testSendAndReceiveAMsg() throws Exception {
+
+        for (String value : new ArrayList<String>(Arrays.asList(new String[]{"key", "secret", "queue"}))) {
+            assertNotNull("value: " + value + " cannot be null or functional test will not work!", System.getProperty(value));
+        }
+
+        SQSAdapter ahoy = new SQSAdapter(System.getProperty("key"), System.getProperty("secret"), System.getProperty("queue"));
+        assertNotNull(ahoy);
+
+        final String origMessage = "this is a test message";
+        final boolean[] wasSent = {false};
+        ahoy.send(origMessage, new MessageSentCallback() {
+            @Override
+            public void onSend(String messageId) {
+                wasSent[0] = true;
+                assertNotNull("message ID from AWS was null!", messageId);
+            }
+        });
+
+        Thread.sleep(2000);
+        assertEquals("callback for sent wasn't invoked", true, wasSent[0]);
+        Thread.sleep(20000);
+
+        final boolean[] wasReceived = {false};
+        ahoy.receive(new MessageReceivedCallback() {
+            @Override
+            public void onReceive(String messageId, String message) {
+                wasReceived[0] = true;
+                assertNotNull("message id was null", messageId);
+                assertEquals("message wasn't " + origMessage, origMessage, message);
+            }
+        });
+
+        Thread.sleep(20000);
+        assertEquals("callback for received wasn't invoked", true, wasReceived[0]);
+    }
+}
